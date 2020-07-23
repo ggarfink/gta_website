afab09212716ede6e11a65d8bc7fa8d91bd16f91
ggarfink <ggarfink@umich.edu>
2020-07-22 22:14:56 -0400

    adding the project

---
+++ minesweeper/minesweeper/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+
+//I know this is bad practice, but I'm lazy
+using namespace std;
+
+//Use this to compile:
+//-Wconversion -Wall -Werror -Wextra -pedantic
+//Add -O3 for efficiency, add -g3 for debugging
+
+int main(int argc, char** argv) {
+	cout << "hello \n";
+	return 0;
+}
